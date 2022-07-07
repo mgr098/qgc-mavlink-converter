5826744592bee130a7671ed85a3ac40078530c96
mgr098 <mgr098@uit.no>
2022-07-07 15:52:34 +0200

    ✨ moved conversion logic into mavlink object

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ import json
 import logging
 import argparse
 
-from numpy import require
+from numpy import append, require
 from constants import * # TODO: change this, bad practice
 
 # inspired by github user @phrohdoh
@@ -56,48 +56,110 @@ class Converter():
         return tabs
 
     def convert_to_mavlink(self):
-
-        mav = MAVlink(LATEST_VERSION)
-
         """Converts plan to mavlink"""
-        mav = [[], []]
-        mav[0].append("QGC WPL" + LATEST_VERSION) # Add meta data
 
-        # Add takeoff first
-        counter = 0
-        cmd_line = [counter, 1, 3, TAKEOFF, 0, 0, 0, 0, 0, 0, 0, 0] # TODO, do this with listcomp
+        mav = MAVlink(self.qgc_plan, LATEST_VERSION, takeoff=True)
+        mav.main()
+        self.write_to_disk(mav.f_mavlink)
+        return 
 
-        # TODO: Move this to a class
-        for i, item in enumerate(self.qgc_plan["mission"]["items"]):
-            params = [i for i in item["params"]]
-            #TODO: check for autoconintue
-            self.line = [i + 1, 0, item["frame"], item["command"], *params, 1 if item["autoContinue"] else 0] # TODO: do this with listcomp
-            self.lines.append(self.line)
+        # mav = []
+        # mav.append("QGC WPL" + LATEST_VERSION) # Add meta data
+
+        # # Add takeoff first
+        # counter = 0
+        # cmd_line = [counter, 1, 3, TAKEOFF, 0, 0, 0, 0, 0, 0, 0, 0] # TODO, do this with listcomp
+
+        # # TODO: Move this to a class
+        # for i, item in enumerate(self.qgc_plan["mission"]["items"]):
+        #     params = [i for i in item["params"]]
+        #     self.line = [i + 1, 0, item["frame"], item["command"], *params, 1 if item["autoContinue"] else 0] # TODO: do this with listcomp
+        #     self.lines.append(self.line)
         
-        self.write_to_disk(mav, cmd_line)
+        # self.write_to_disk(mav, cmd_line)
 
-    def write_to_disk(self, mav, cmd_line):
+    def write_to_disk(self, mav):
         #TODO: Move this to a function 
+
+
+
+        # for i in self.lines:
+        #     i.append("\n")
+        
+        
+        # print(self.lines)
+        # skr = []
+        # for i in self.lines:
+        #     tabbed = self.tab_it(i)
+        #     for j in tabbed:
+        #         skr.append(j)
+        # o = [*mav, self.tab_it(cmd_line)]
+                
+        # for i in self.lines:
+        #     tabbed = self.tab_it(i)
+        #     o.append(tabbed)
+
+        # print(o)
+
         with open("output.mavlink", "w+") as f:
-            f.write(str(*mav[0]))
-            f.write("\n")
+            # f.write(str(mav))
+            # f.write("\n")
 
-            cmd_line = self.tab_it(cmd_line)
-            f.write(cmd_line)
+            # cmd_line = self.tab_it(cmd_line)
+            # f.write(cmd_line)
 
-            for line in self.lines:
-                f.write("\n")
-                f.write(self.tab_it(line))
-            
+            # for line in self.lines:
+            #     f.write("\n")
+            #     f.write(self.tab_it(line))
+            for i in mav:
+                f.write(str(i))
+                # f.write("\n")
             f.close()
 
-"""MAVlink object"""
 class MAVlink():
-    def __init__(self, version) -> None:
+    def __init__(self, plan, version, takeoff):
+        self.plan = plan
+
         self.header = "QGC WPL {}".format(version)
+        self.takeoff = [0, 1, 3, TAKEOFF, 0, 0, 0, 0, 0, 0, 0, 0] if takeoff == True else None # TODO, do this with listcomp
+        self.mission_items = self.populate()
+
+        self.mavlink = [self.header, *self.takeoff, *self.mission_items] 
+        self.f_mavlink = self.format_mavlink()
 
     def main(self):
-        print(self.header)
+        print(self.f_mavlink)
+    
+    def format_mavlink(self):
+        
+        f_mavlink = []
+
+        f_mavlink.append(self.header)
+        f_mavlink.append("\n")
+        f_mavlink.append(self.tab_it(self.takeoff))
+
+        for line in self.mission_items:
+            tabbed_line = self.tab_it(line)
+            f_mavlink.append("\n")
+            f_mavlink.append(tabbed_line)
+
+        return f_mavlink
+
+    def populate(self):
+        #TODO: better names? maybe restructure, hard to read?
+        """Populates MAVlink object according to plan"""
+        
+        mission_items = []
+        for i, item in enumerate(self.plan["mission"]["items"]):
+            params = [i for i in item["params"]] 
+            mission_item = [i + 1, 0, item["frame"], item["command"], *params, 1 if item ["autoContinue"] else 0]
+            mission_items.append(mission_item)
+        
+        return mission_items
+
+    def tab_it(self, target):
+        """Insert tab between every item in target"""
+        return "\t".join(str(t) for t in target)
 
 if __name__ == "__main__":
     args = parse_args()
