7daa36767664523c1b31ba6f2731e00431cfe710
mgr098 <mgr098@uit.no>
2022-07-08 11:27:54 +0200

    🎨 renaming, reformat code,

--- src/main.py
+++ src/main.py
@@ -1,97 +1,105 @@
-from fileinput import filename
-import string
-import sys
 import json
 import logging
 import argparse
+from constants import LATEST_VERSION, TAKEOFF, DEFAULT_FILE_NAME
 
-from numpy import append, require
-from constants import * # TODO: change this, bad practice
-
-# inspired by github user @phrohdoh
 # TODO: try without takeoff, try many different plans to test
+
+# logger=logging.getLogger()
+# logger.setLevel(logging.DEBUG)
 
 def parse_args():
     #TODO: create better names
-    # Add takeoff off option
-    parser = argparse.ArgumentParser()
     parser = argparse.ArgumentParser(description="Convert QGC .plan to .mavlink format")
-    parser.add_argument("filename", type=str, help="Usage: python3 main.py </path/to/file/>")
-    parser.add_argument("--out", type=str, help=".mavlink filename", default=DEFAULT_FILE_NAME)
-    args = parser.parse_args()
 
-    return args
+    parser.add_argument(
+        "filepath", type=str, help="Usage: python3 main.py </path/to/file/>")
+    parser.add_argument(
+        "--out", type=str, help="MAVlink filename", default=DEFAULT_FILE_NAME)
+    parser.add_argument(
+        "--version", type=str, help="MAVlink version", default=LATEST_VERSION)
+    parser.add_argument(
+        "--takeoff", type=str, help="Add takeoff at start of mavlink", default=False)
+
+    return parser.parse_args()
+
 
 class Converter():
-    def __init__(self, filename, out):
-        self.filename = filename 
+    def __init__(self, filepath, out, takeoff, version):
+        self.filepath = filepath 
         self.out = out
-        self.qgc_plan = {}
+        self.takeoff = takeoff
+        self.version = version
+        self.plan = {}
 
     def main(self):
-        self.verify_plan()
-        self.convert_to_mavlink()
+        self.verify_format()
+        mav = Mav(self.plan, self.version, self.takeoff)
+        self.write_to_disk(mav.file)
+        print("Successfully converted {} to {}".format(self.filepath, self.out))
 
-    def verify_plan(self):
+    def verify_format(self):
         """Verifies plan format"""
+        #TODO: check if its a plan, and that its not empty?
         try:
-            with open(self.filename) as f:
-                self.qgc_plan = json.load(f)
+            with open(self.filepath) as f:
+                self.plan = json.load(f)
                 f.close()
         except FileNotFoundError:
             logging.error("Can't open specified file")
+        except:
+            logging.error("Unexpected error")
 
-        #TODO: check if its a plan, and that its not empty?
+    def write_to_disk(self, mav):
+        """Write mavlink object to file"""
+        try:
+            with open(self.out, "w+") as f:
+                for line in mav:
+                    f.write(str(line))
+                f.close()
+        except:
+            logging.exception("Unexpected error, could not append MAVlink object to file")
 
-    def convert_to_mavlink(self):
-        """Converts plan to mavlink"""
 
-        mav = MAVlink(self.qgc_plan, LATEST_VERSION, takeoff=True)
-        self.write_to_disk(mav.f_mavlink)
-
-    def write_to_disk(self, mavlink):
-        """Writes object down to disk"""
-        #TODO: add try/except
-        with open(self.out, "w+") as f:
-            for line in mavlink:
-                f.write(str(line))
-            f.close()
-
-class MAVlink():
+class Mav():
     def __init__(self, plan, version, takeoff):
         self.plan = plan
         self.header = "QGC WPL {}".format(version)
-        self.takeoff = [0, 1, 3, TAKEOFF, 0, 0, 0, 0, 0, 0, 0, 0] if takeoff == True else None # TODO, do this with listcomp
-        self.mission_items = self.populate()
-        self.f_mavlink = self.format_mavlink()
+        self.takeoff = takeoff
+        
+        self.mission_items = self.convert()
+        self.file = self.format_items()
     
-    def format_mavlink(self):
-        f_mavlink = []
+    def format_items(self):
+        mav_file = []
+        mav_file.append(self.header)
 
-        f_mavlink.append(self.header)
-        f_mavlink.append("\n")
-        f_mavlink.append(self.tab_it(self.takeoff))
+        if self.takeoff:
+            self.takeoff = [0, 1, 3, TAKEOFF, 0, 0, 0, 0, 0, 0, 0, 0]
+            mav_file.append("\n")
+            mav_file.append(self.insert_tabs(self.takeoff))
 
         for line in self.mission_items:
-            tabbed_line = self.tab_it(line)
-            f_mavlink.append("\n")
-            f_mavlink.append(tabbed_line)
+            mav_file.append("\n")
+            mav_file.append(self.insert_tabs(line))
 
-        return f_mavlink
+        return mav_file
 
-    def populate(self):
-        #TODO: better names? maybe restructure, hard to read?
-        """Populates MAVlink object according to plan"""
-        
+    def convert(self):
+        """Convert plan according to MAVlink"""
+        #TODO: rewrite maybe?
         mission_items = []
+
         for i, item in enumerate(self.plan["mission"]["items"]):
             params = [i for i in item["params"]] 
-            mission_item = [i + 1, 0, item["frame"], item["command"], *params, 1 if item ["autoContinue"] else 0]
+            mission_item = [i + 1, 0, item["frame"], item["command"], *params, 
+                            1 if item ["autoContinue"] else 0]
+
             mission_items.append(mission_item)
         
         return mission_items
 
-    def tab_it(self, target):
+    def insert_tabs(self, target):
         """Insert tab between every item in target"""
         return "\t".join(str(t) for t in target)
 
@@ -99,5 +107,5 @@ class MAVlink():
 if __name__ == "__main__":
     args = parse_args()
 
-    converter = Converter(args.filename, args.out)
+    converter = Converter(args.filepath, args.out, args.takeoff, args.version)
     converter.main()
