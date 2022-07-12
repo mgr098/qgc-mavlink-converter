fea72dd5e29a90f8e59a384cc6f6ac5c3b6baac4
mgr098 <mgr098@uit.no>
2022-07-12 09:17:15 +0200

    ✨ added parser

--- src/upload_mavlink.py
+++ src/upload_mavlink.py
@@ -1,18 +1,14 @@
-import sys
+import os
+import requests
+import argparse
+
 import olympe
 from olympe.messages.common.Mavlink import Start
-from olympe.messages.common.MavlinkState import (
-    MavlinkFilePlayingStateChanged,
-    MissionItemExecuted,
-)
 
-import requests
-import os
 
-#TODO: Add arg parser
-olympe.log.update_config({"loggers": {"olympe": {"level": "ERROR"}}})
+olympe.log.update_config({"loggers": {"olympe": {"level": "INFO"}}})
 
-drone_ip = "10.202.0.1"
+DRONE_IP = "10.202.0.1"
 
 headers = {
     "Accept": "application/json, text/javascript, text/plain */*; q=0.01",
@@ -20,22 +16,39 @@ headers = {
     "Content-type": "application/json; charset=UTF-8; application/gzip",
 }
 
-drone = olympe.Drone(drone_ip)
-drone.connect()
+def parse_args():
+    parser = argparse.ArgumentParser(description="Upload and start AirSDK flightmission with mavlink file")
 
-filepath = sys.argv[1]
+    parser.add_argument(
+        "filepath", type=str, help="Usage: python3 upload_mavlink.py </path/to/file/>")
+    parser.add_argument(
+        "--ip", type=str, help="Drone ip address", default=DRONE_IP)
 
-# Upload mavlink file
-with open(filepath, "rb") as data:
-    resp = requests.put(
-        url=os.path.join("http://", drone_ip, "api/v1/upload", "flightplan"),
-        headers=headers,
-        data=data,
-    )
+    return parser.parse_args()
+    
+def main(filepath, drone_ip):
+    drone = olympe.Drone(drone_ip)
+    drone.connect()
 
-# Start flightplan
-expectation = drone(
-    Start(resp.json(), type="flightPlan")
-).wait(_timeout=200)
+    # Upload mavlink file
+    with open(filepath, "rb") as data:
+        resp = requests.put(
+            url=os.path.join("http://", drone_ip, "api/v1/upload", "flightplan"),
+            headers=headers,
+            data=data,
+        )
+    
+    # Start flightplan
+    try:
+        drone(
+            Start(resp.json(), type="flightPlan")
+        ).wait(_timeout=200)
+    except:
+        print("Error could not start flightplan")
 
-assert expectation
+    drone.disconnect()
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    main(args.filepath, args.ip)
